f5164f17187ecab82ce7bec587421fb5a352ad52
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-01-01 21:48:16 -0300

    Chore: Init project Next.js and configure stitches

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Heading, Text } from '@ignite-ui/react'
+
+const Home = () => {
+  return (
+    <div>
+      <Heading as="h1">Welcome to Ignite Call</Heading>
+    </div>
+  )
+}
+
+export default Home
