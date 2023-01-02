a69bf7bfba45bdd377dae2eecd893e6a5b50ffea
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-01-01 22:33:15 -0300

    Chore: Configure page extensions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+/* eslint-disable prettier/prettier */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
