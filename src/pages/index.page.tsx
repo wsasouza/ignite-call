3737b856fdfa5e21f554c3a9f39e22f7c1280cf8
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-01-02 22:52:35 -0300

    Feat: Create Home

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,1 @@
-import { Heading } from '@ignite-ui/react'
-
-const Home = () => {
-  return (
-    <div>
-      <Heading as="h1">Welcome to Ignite Call</Heading>
-    </div>
-  )
-}
-
-export default Home
+export { default } from './home'
