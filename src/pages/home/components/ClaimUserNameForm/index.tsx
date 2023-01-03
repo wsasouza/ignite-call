40ffa8bb1c124efded0d848d9a957024415903f5
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-01-02 23:40:25 -0300

    Bug: style app

--- src/pages/home/components/ClaimUserNameForm/index.tsx
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -1,11 +1,30 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Form } from './styles'
 
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type claimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
 export function ClaimUserNameForm() {
+  const { register, handleSubmit } = useForm<claimUsernameFormData>()
+
+  async function handleClaimUsername(data: claimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
