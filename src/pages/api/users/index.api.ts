2f0f9a40f1a137b11af4ec35c41c5d5b14d04870
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-01-10 22:38:09 -0300

    Feat: Create cookie

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
 import { prisma } from '../../../lib/prisma'
 
 export default async function handler(
@@ -12,11 +13,28 @@ export default async function handler(
 
   const { name, username } = req.body
 
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'Username already exists.',
+    })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7days
+    path: '/',
   })
 
   return res.status(201).json(user)
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -8,6 +8,7 @@ import { ArrowRight } from 'phosphor-react'
 import { Container, Form, FormError, Header } from './styles'
 import { useEffect } from 'react'
 import { api } from '../../lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -49,6 +50,11 @@ export default function Register() {
         username: data.username,
       })
     } catch (err) {
+      if (err instanceof AxiosError && err.response.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+
       console.log(err)
     }
   }
