22467a207c891e8fbea8935e63dfd0fa9cfc0154
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-01-15 19:39:51 -0300

    Fix: configure scopes

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,6 +1,5 @@
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-import { signIn } from 'next-auth/react'
 
 export const authOptions: NextAuthOptions = {
   providers: [
